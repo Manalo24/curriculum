8d8672d3a505293cd0618cb70b83cd9c4dacc9bf
4G-IT <4G-IT@INFO1-PC12>
2018-12-03 13:52:14 +0100

    asa

--- curicculum/src/curicculum/principale.java
+++ curicculum/src/curicculum/principale.java
@@ -5,12 +5,54 @@ public class principale {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
+		int pick;
+		int scelta=0;
 		long id;
 		LocalDate dataInzio;
 		LocalDate dataFine;
 		String descrizione;
 		
+		int finale=0;
+		while(finale==0) {
+		System.out.println("0)Exit");
+		System.out.println("1)Inserimento CV");
+		System.out.println("2)Elenco");
+		System.out.println("3)Posti");
+		System.out.println("4)Togliere");
+		pick=input.nextInt();
+		switch(pick) {
+		case 0: finale=1;
+				break;
+		case 1: 
+			System.out.println("Scegli:");
+			System.out.println("1)Esperienza Lavorativa");
+			System.out.println("2)Studio Effetuato");
+			scelta=input.nextInt();
+			if (scelta==1) {
+				System.out.println("1)Inserisci ID");
+				id=input.nextLong();
+				System.out.println("2)Inserisci Data Inizio");
+
+			}
+			
+				break;
+		case 2: 
+			
+			
+				break;
+		case 3: 
+			
+			
+				break;
+		case 4: 
+			
+			
+				break;
+		
+		}
+		
+		}
 	}
 
 }
